66941f2363553d7dadb51e36a3c96855496ccbc3
Philippe Gerber <philippe@bigwhoop.ch>
2015-05-12 21:28:01 +0200

    WIP

--- data/filter.php
+++ data/filter.php
@@ -4,10 +4,18 @@ foreach (glob(__DIR__ . '/*.txt') as $file) {
     $data = str_replace("\r", '', $data);
     
     $lines = explode("\n", $data);
+    
     $lines = array_map(function($line) {
         list($line) = explode('--', $line);
         return trim($line);
     }, $lines);
+    
+    $lines = array_filter($lines, function($line) {
+        return '.' === substr($line, -1);
+    });
+    
+    $lines = array_unique($lines);
+    asort($lines);
     
     $data = join("\n", $lines);
     
